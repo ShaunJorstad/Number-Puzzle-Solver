7135deeb23924ebb116fb369c6494b248cc81378
Shaun Jorstad <jorstadsd17@gcc.edu>
2021-03-05 20:32:51 -0500

    shuffleValid

--- board.py
+++ board.py
@@ -1,15 +1,21 @@
 import collections
 import math
 from tabulate import tabulate
+import os
+import sys
+import random
+import time
 
 
 class Board(collections.MutableSequence):
-    def __init__(self, size):
-        self.slots = list()
-        self.size = size
-        for i in range(size):
-            self.slots.append(i)
-        self.slots[0] = ' '
+    def __init__(self, size, presetList=None):
+        if presetList != None:
+            self.slots = presetList
+        else:
+            self.slots = list()
+            self.size = size
+            for i in range(size):
+                self.slots.append(i)
 
     def __len__(self):
         return len(self.slots)
@@ -29,17 +35,60 @@ class Board(collections.MutableSequence):
     def append(self, value):
         self.insert(len(self) + 1, value)
 
+    def customBuild(self):
+        usedValues = []
+        validRange = list(range(self.size))
+        for i in range(self.size):
+            self.slots[i] = ' '
+        for i in range(self.size):
+            os.system('cls' if os.name == 'nt' else 'clear')
+            self.slots[i] = '#'
+            print(self)
+            inputVal = -1
+            print(set(validRange) - set(usedValues))
+            while inputVal not in validRange or inputVal in usedValues:
+                try:
+                    inputVal = input(f'Index {i} number: ')
+                    inputVal = int(inputVal)
+                except:
+                    if inputVal == 'quit':
+                        sys.exit()
+                    print('invalid input: enter "quit" to exit')
+            self.slots[i] = inputVal
+            usedValues.append(inputVal)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(self)
+
     def swap(self, i, j):
         a = self.slots[i]
         b = self.slots[j]
         self.slots[i] = b
-        self.slots[b] = a
+        self.slots[j] = a
 
     def chunks(self):
         """Yield successive n-sized chunks from lst."""
         bp = int(math.sqrt(self.size))
         for i in range(0, len(self.slots), bp):
-            yield self.slots[i:i + bp]
+            yield (val if val != 0 else ' ' for val in self.slots[i:i + bp])
+
+    def shuffle(self):
+        random.shuffle(self.slots)
+
+    def shuffleValid(self):
+        inversions = random.randrange(14, 80, 2)
+        for i in range(inversions):
+            a = random.randrange(0, self.size)
+            if a == 0:
+                self.swap(0, 1)
+            elif a == self.size - 1:
+                pass
+                self.swap(self.size - 1, self.size-2)
+            else:
+                direction = random.randrange(0, 2)
+                if direction == 0:
+                    self.swap(a, a-1)
+                else:
+                    self.swap(a, a+1)
 
     def __str__(self):
         return tabulate(self.chunks(), tablefmt="fancy_grid")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from idfs import idfs
 from ass import ass
 from board import Board
+import os
 
 algorithms = {
     '': ('idfs and A*', [idfs, ass]),
@@ -31,33 +32,37 @@ def prompt(validInput, promptTitle, default):
 
 def promptAlgorithm():
     ''' prompts the user for the algorithm to run'''
+    os.system('cls' if os.name == 'nt' else 'clear')
     return prompt(algorithms, 'Select which algorithm to run', 'all')
 
 
 def promptLoggingLevel():
     ''' prompts the user for the logging level to use'''
+    os.system('cls' if os.name == 'nt' else 'clear')
     return prompt(loggingLevels, 'Select logging level', 'silent')
 
 
 def useCustomBoard():
     ''' prompts the user if they want provide a custom board to solve or not'''
+    os.system('cls' if os.name == 'nt' else 'clear')
     return '' != prompt({'yes': 'y', '': 'no'}, f'Solve custom board\n(main algorithm analytics are not run on custom boards)', 'no')
 
 
 if __name__ == '__main__':
+    boardSize = 9
     alg = promptAlgorithm()
     loggingLevel = promptLoggingLevel()
     customBoard = useCustomBoard()
-    board = None
+    boards = []
     if customBoard:
-        print('todo: prompt user through creating custom board')
+        board = Board(boardSize)
+        board.customBuild()
+        boards.append(board)
+    else:
+        # reference a list of predefined boards
+        pass
     print(
         f'\nRunning {algorithms[alg][0]} at {loggingLevels[loggingLevel]} logging')
     for algorithm in algorithms[alg][1]:
-        solver = algorithm(loggingLevel, board)
+        solver = algorithm(loggingLevel, boards)
         solver.run()
-
-    test = Board(9)
-    test[8] = 400
-    test.swap(3, 4)
-    print(test)
